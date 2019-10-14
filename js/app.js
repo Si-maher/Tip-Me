ac76221ce19795be9647e9218aae74ccdac48c59
Simon Maher <simonmaher8307@hotmail.com>
2019-10-14 13:23:08 +0300

    added value options

---
+++ js/app.js
@@ -0,0 +1,18 @@
+// Immediately invoked function 
+(function() {
+// select options 
+const services = [
+    {
+        value:1,
+        title:'great - 20%'
+    },
+    {
+        value:2,
+        title:'great - 10%'
+    },
+    {
+        value:3,
+        title:'great - 2%'
+    },
+]
+})()
